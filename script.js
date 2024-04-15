5697bc9ded00b009d91d9cd25068fa3a104173ea
Harshit Sachdeva <105371858+harshit070104@users.noreply.github.com>
2024-04-15 17:14:01 +0530

    Update script.js
    Corrected some logic and added new error detection

--- script.js
+++ script.js
@@ -23,23 +23,22 @@ function hideResult(){
 
 function check(id , errorId){ // Checking the input field so that it shouldnot contain characters other than numbers
     let income = document.getElementById(id).value;
-    let size = income.length;
+    let size = income.length;   
     
     let nums = ["0","1","2","3","4","5","6","7","8","9","."];
     let ele = document.getElementById(id);
     for(let i=size-1 ; i>=0 ; i--)  // Untill the input contains char other than numbers show error
     {
-
         if(!nums.includes(income[i])){
             document.getElementById(errorId).style.display = "block";   // Displaying error
             ele.style.outline = "solid 2px rgba(255, 0, 0, 0.598)"; // Giving warning
-            ele.style.borderStyle = "none"; // Removing border
+            ele.style.borderStyle = "none"; // Removing border            
             return;    
         }
-    }
+    }    
     document.getElementById(errorId).style.display = "none";    // Remove error
     ele.style.outlineStyle = "none";    // Removing red warning outline    
-    ele.style.borderStyle = "solid";    // Adding back border   
+    ele.style.borderStyle = "solid";    // Adding back border       
 }
 
 function valid(){
@@ -57,14 +56,14 @@ function valid(){
 }
 
 function calculateTax(){    // Function for calculating result     
-    let annualIncome = document.getElementById("annualIncome").value;
-    let size = annualIncome.length
+    let annualIncome = document.getElementById("annualIncome").value;    
     if(annualIncome.length == 0){
         annualIncome = 0;
     }
     else{
         annualIncome = parseInt(annualIncome);
     }
+    console.log(annualIncome);        
 
     let extraIncome = document.getElementById("extraIncome").value;
     if(extraIncome.length == 0){
@@ -72,15 +71,17 @@ function calculateTax(){    // Function for calculating result
     }
     else{
         extraIncome = parseInt(extraIncome);
-    }
-
+    }    
+    console.log(extraIncome);
+    
     let deductions = document.getElementById("deductions").value;
     if(deductions.length == 0){
         deductions = 0;
     }
-    else{        
+    else{                
         deductions = parseInt(deductions);
     }
+    console.log(deductions);
 
     let age = document.getElementById("selectAge").value;
     if(age == ''){        
@@ -89,8 +90,9 @@ function calculateTax(){    // Function for calculating result
     else{
         document.getElementById("error3").style.display = "none";
     }   
+    console.log(age);
     
-    let overallIncome = annualIncome+extraIncome-deductions;
+    let overallIncome = annualIncome+extraIncome-deductions;    
     let taxableIncome = overallIncome-800000;
     let netIncome;    
     if(taxableIncome>0){
@@ -108,10 +110,11 @@ function calculateTax(){    // Function for calculating result
     }
     else{
         netIncome = overallIncome;
-    }    
-    
-    
+    } 
+
+    let size = Math.floor(Math.log10(netIncome) + 1); 
     netIncome = netIncome.toPrecision(size+2);
+
     let i=size-1;
     let threeCount = 2;
     let twoCount = 1;
@@ -128,20 +131,22 @@ function calculateTax(){    // Function for calculating result
             i--;    // As we dont wanna consider comma so thats why
         }
 
-
         while(i>0 && twoCount--){
             i--;
         }
-        if(i>0){
-            console.log(i);
+        if(i>0){            
             netIncome = netIncome.slice(0, i) + "," + netIncome.slice(i);
         }
         i--;
         twoCount = 1;
     }
-    netIncome = "₹" + netIncome
+    netIncome = "₹" + netIncome;
 
-
+    // If error in any of the field is still there it will not show the result
+    if(document.getElementById("error1").style.display == "block" || document.getElementById("error3").style.display == "block" || document.getElementById("error4").style.display == "block"){           
+        return;
+    }
+    
     document.getElementById("netIncome").innerHTML = netIncome;
     showResult();
 }
